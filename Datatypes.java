bb1fe9311302b24b47ba39dbc7620cd34cd88cb2
Krishna Singhal <singhalkrishna790@gmail.com>
2026-01-30 10:05:27 +0530

    first commit

---
+++ Datatypes.java
@@ -0,0 +1,10 @@
+class Datatypes{
+    public static void main(String[] args){
+        int a=10;
+        float b=0.23f;
+        double c=0.3333;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
